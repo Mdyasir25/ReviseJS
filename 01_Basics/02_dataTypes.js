d3973c89e90f2081f777baea589bd2e89fdb4a41
Md Yasir <mdyasir25@gmail.com>
2024-08-20 11:08:29 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let demo = null
+let name = "Yasir"
+let age = 26
+let isLoggedIn = true
+let state;
+
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof null); //output object
+console.log(typeof undefined); //output undefined
